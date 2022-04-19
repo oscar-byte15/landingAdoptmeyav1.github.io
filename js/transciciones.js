be3feab55b48305cff854e860ea5ab83ff4788ed
daniel <presidente152016@outlook.com>
2022-04-19 18:50:02 -0500

    f: Adding new files v1

---
+++ js/transciciones.js
@@ -0,0 +1,74 @@
+window.sr=ScrollReveal({
+    reset:true
+});
+
+sr.reveal('.top', {
+    duration: 2000,
+    origin: 'bottom',
+    distance: '-100px'
+ 
+});
+
+
+
+sr.reveal('.v1', {
+    duration: 2000,
+    origin: 'left',
+    distance: '400px'
+
+});
+
+sr.reveal('.img1', {
+    duration: 2000,
+    origin: 'top',
+    distance: '400px'
+
+});
+
+sr.reveal('.myChart', {
+    duration: 3000,
+    origin: 'left',
+    distance: '400px'
+
+});
+
+sr.reveal('.img3', {
+    duration: 2000,
+    origin: 'left',
+    distance: '400px'
+
+});
+sr.reveal('.quienes-somos', {
+    duration: 3000,
+    origin: 'left',
+    distance: '400px'
+
+});
+
+
+sr.reveal('.img2', {
+    duration: 3000,
+    origin: 'top',
+    distance: '400px'
+
+});
+
+sr.reveal('.cobertura', {
+    duration: 3000,
+    origin: 'left',
+    distance: '400px'
+
+});
+sr.reveal('.pricing', {
+    duration: 3000,
+    origin: 'left',
+    distance: '400px'
+
+});
+
+sr.reveal('.footer', {
+    duration: 3000,
+    origin: 'top',
+    distance: '400px'
+
+});
